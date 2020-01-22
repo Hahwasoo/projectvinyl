83bafa3cb48bd596edeb439aa566310ed1339145
Hahwasoo <gung4060@gmail.com>
2020-01-22 14:37:17 +0900

    진영 header,footer 수정

--- src/jy/com/homedream/controller/JY_JN_ListAction.java
+++ src/jy/com/homedream/controller/JY_JN_ListAction.java
@@ -102,7 +102,7 @@ public class JY_JN_ListAction implements Action {
 		
 		ActionForward forward = new ActionForward();
 		forward.setForward(true);
-		forward.setUrl("/jy_jn/jy_jn_list.jsp");
+		forward.setUrl("/hs_communitymain.jsp?page=jy_jn/jy_jn_list.jsp");
 		
 		return forward;
 	}
